c2015844d9edd95ed36ff460365d047af34ab3a7
Fell <fell@fellr.net>
2019-01-26 06:45:48 +0100

    more rythm game work

--- Assets/RythmGame/Scripts/RythmGameController.cs
+++ Assets/RythmGame/Scripts/RythmGameController.cs
@@ -12,10 +12,18 @@ public enum NoteType
     Y
 }
 
-public struct Note
+public class Note
 {
+    public Note(float time, NoteType type)
+    {
+        this.hitTime = time;
+        this.type = type;
+    }
+
     public float hitTime;
     public NoteType type;
+    public GameObject noteObject;
+    public bool delete;
 }
 
 public class RythmGameController : MonoBehaviour
@@ -26,46 +34,42 @@ public class RythmGameController : MonoBehaviour
     public GameObject noteXPrefab;
     public GameObject noteYPrefab;
 
-    public float noteSpeed = 1;
-    public float noteRampTime = 0.5f;
+    float noteRampTime = 1;
 
     float timer = 0.0f;
 
     Queue<Note> beatmap;
-    List<GameObject> activeNotes = new List<GameObject>();
-    
+    List<Note> activeNotes = new List<Note>();
+
     Note nextNote;
+    Note lastNote;
 
     // Start is called before the first frame update
     void Start()
     {
-        //TODO(Felix): Clear this!
         beatmap = new Queue<Note>();
 
-        Note n = new Note();
-        n.hitTime = 1.0f;
-        n.type = NoteType.A;
-        beatmap.Enqueue(n);
+        beatmap.Enqueue(new Note(1f, NoteType.A));
+        beatmap.Enqueue(new Note(1.4f, NoteType.A));
+        beatmap.Enqueue(new Note(1.8f, NoteType.A));
+        beatmap.Enqueue(new Note(2.0f, NoteType.A));
+        beatmap.Enqueue(new Note(2.2f, NoteType.B));
 
-        n = new Note();
-        n.hitTime = 1.5f;
-        n.type = NoteType.A;
-        beatmap.Enqueue(n);
+        beatmap.Enqueue(new Note(2.6f, NoteType.A));
+        beatmap.Enqueue(new Note(3f, NoteType.X));
+        beatmap.Enqueue(new Note(3.4f, NoteType.A));
+        beatmap.Enqueue(new Note(3.8f, NoteType.B));
 
-        n = new Note();
-        n.hitTime = 2f;
-        n.type = NoteType.A;
-        beatmap.Enqueue(n);
+        beatmap.Enqueue(new Note(4.2f, NoteType.A));
+        beatmap.Enqueue(new Note(4.6f, NoteType.A));
+        beatmap.Enqueue(new Note(5f, NoteType.A));
+        beatmap.Enqueue(new Note(5.2f, NoteType.A));
+        beatmap.Enqueue(new Note(5.4f, NoteType.B));
 
-        n = new Note();
-        n.hitTime = 2.5f;
-        n.type = NoteType.B;
-        beatmap.Enqueue(n);
-
-        n = new Note();
-        n.hitTime = 3f;
-        n.type = NoteType.A;
-        beatmap.Enqueue(n);
+        beatmap.Enqueue(new Note(5.8f, NoteType.A));
+        beatmap.Enqueue(new Note(6.2f, NoteType.X));
+        beatmap.Enqueue(new Note(6.6f, NoteType.A));
+        beatmap.Enqueue(new Note(7.0f, NoteType.B));
 
         nextNote = beatmap.Dequeue();
     }
@@ -76,31 +80,66 @@ public class RythmGameController : MonoBehaviour
         float deltaTime = Time.deltaTime;
         timer += deltaTime;
 
-        if(timer > nextNote.hitTime - noteRampTime && beatmap.Count > 0)
+        // Spawn overdue notes
+        if(nextNote != null && timer > nextNote.hitTime - noteRampTime)
         {
-            switch(nextNote.type)
-            {
-                case NoteType.A:
-                    GameObject note = GameObject.Instantiate(noteAPrefab, transform);
-                    note.GetComponent<RectTransform>().anchoredPosition = new Vector2(-600, 0);
-                    activeNotes.Add(note);
-                    break;
-                case NoteType.B:
-                    GameObject note2 = GameObject.Instantiate(noteBPrefab, transform);
-                    note2.GetComponent<RectTransform>().anchoredPosition = new Vector2(-600, 0);
-                    activeNotes.Add(note2);
-                    break;
-            }
+            SpawnNote(nextNote);
 
             if(beatmap.Count > 0)
                 nextNote = beatmap.Dequeue();
+            else
+                nextNote = null;
         }
 
         // Update active notes
-        foreach(GameObject note in activeNotes)
+        foreach(Note note in activeNotes)
         {
-            Vector3 velocity = Vector3.Normalize(rythmTarget.transform.localPosition - note.transform.localPosition) * noteSpeed * deltaTime;
-            note.transform.Translate(velocity);
+            float diff = timer - note.hitTime;
+            note.noteObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(-1060 * (diff / noteRampTime) * -1, 0);
+
+            // Remove off-screen notes
+            if(diff > noteRampTime)
+            {
+                Destroy(note.noteObject);
+                note.delete = true;
+            }
+                
+
+            // Remove hit notes?
+            if(Input.GetKeyDown(KeyCode.K))
+            {
+                if(diff < 0.100f && diff > -0.100f)
+                {
+                    Destroy(note.noteObject);
+                    note.delete = true;
+                }
+            }
         }
+
+        activeNotes.RemoveAll(n => n.delete);        
+    }
+
+    void SpawnNote(Note note)
+    {
+        GameObject prefab = noteAPrefab;
+        switch(note.type)
+        {
+            case NoteType.A:
+                prefab = noteAPrefab;
+                break;
+            case NoteType.B:
+                prefab = noteBPrefab;
+                break;
+            case NoteType.X:
+                prefab = noteXPrefab;
+                break;
+            case NoteType.Y:
+                prefab = noteYPrefab;
+                break;
+        }
+
+        GameObject noteObject = GameObject.Instantiate(prefab, transform);
+        note.noteObject = noteObject;
+        activeNotes.Add(note);
     }
 }
